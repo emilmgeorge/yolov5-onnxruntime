ff1ddf61b7cffe10e2b3043007b56d2eac1aa0af
emil.m <emil.m@dicortech.com>
2023-03-01 16:51:47 +0530

    Add support for video/webcam inference

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,34 @@ int main(int argc, char* argv[])
 {
     const float confThreshold = 0.3f;
     const float iouThreshold = 0.4f;
+    constexpr double avgalpha = 0.1;
 
     cmdline::parser cmd;
     cmd.add<std::string>("model_path", 'm', "Path to onnx model.", true, "yolov5.onnx");
-    cmd.add<std::string>("image", 'i', "Image source to be detected.", true, "bus.jpg");
+    cmd.add<std::string>("image", 'i', "Image source to be detected.", false);
+    cmd.add<std::string>("video", 'v', "Video/webcam source to be detected.", false);
     cmd.add<std::string>("class_names", 'c', "Path to class names file.", true, "coco.names");
     cmd.add("gpu", '\0', "Inference on cuda device.");
 
     cmd.parse_check(argc, argv);
 
     bool isGPU = cmd.exist("gpu");
+    bool isImage = cmd.exist("image");
+    bool isVideo = cmd.exist("video");
     const std::string classNamesPath = cmd.get<std::string>("class_names");
     const std::vector<std::string> classNames = utils::loadNames(classNamesPath);
     const std::string imagePath = cmd.get<std::string>("image");
+    const std::string videoPath = cmd.get<std::string>("video");
     const std::string modelPath = cmd.get<std::string>("model_path");
+
+    double avgduration = 0;
+
+    if(!isImage && !isVideo) {
+        std::cerr << "Either the --image or the --video argument has to be specified." << std::endl;
+        std::cerr << std::endl;
+        std::cerr << cmd.usage();
+        return -1;
+    }
 
     if (classNames.empty())
     {
@@ -32,28 +46,68 @@ int main(int argc, char* argv[])
     }
 
     YOLODetector detector {nullptr};
-    cv::Mat image;
-    std::vector<Detection> result;
-
-    try
-    {
+    try {
         detector = YOLODetector(modelPath, isGPU, cv::Size(640, 640));
-        std::cout << "Model was initialized." << std::endl;
-
-        image = cv::imread(imagePath);
-        result = detector.detect(image, confThreshold, iouThreshold);
-    }
-    catch(const std::exception& e)
-    {
+    } catch(const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return -1;
     }
+    std::cout << "Model was initialized." << std::endl;
 
-    utils::visualizeDetection(image, result, classNames);
+    cv::VideoCapture cap;
+    if(isVideo) {
+        try {
+            cap = cv::VideoCapture(std::stoi(videoPath));
+        } catch (std::logic_error &e) {
+            cap = cv::VideoCapture(videoPath);
+        }
+    }
 
-    cv::imshow("result", image);
-    // cv::imwrite("result.jpg", image);
-    cv::waitKey(0);
+    do {
+        auto start = std::chrono::steady_clock::now();
+
+        cv::Mat image;
+        std::vector<Detection> result;
+
+        try {
+            if(isImage) {
+                image = cv::imread(imagePath);
+            } else {
+                cap >> image;
+            }
+        } catch(cv::Exception ex) {
+            std::cout << ex.what() << std::endl;
+        } catch(...) {
+            std::cout << "Unknown exception" << std::endl;
+        }
+
+        try {
+            result = detector.detect(image, confThreshold, iouThreshold);
+        } catch(const std::exception& e) {
+            std::cerr << e.what() << std::endl;
+            return -1;
+        }
+
+        utils::visualizeDetection(image, result, classNames);
+
+        cv::imshow("result", image);
+        auto finish = std::chrono::steady_clock::now();
+
+        auto key = cv::waitKey(isVideo) & 0xFF;
+        if(key == 'q' && isVideo)
+            break;
+
+        if(isVideo)
+            finish = std::chrono::steady_clock::now();
+        double duration = std::chrono::duration_cast<std::chrono::duration<double>>(finish - start).count();
+
+        if(isVideo) {
+            avgduration = (avgalpha * duration) + (1.0 - avgalpha) * avgduration;
+            std::cout << 1.0 / (avgduration + 0.0001) << " fps" << std::endl;
+        } else {
+            std::cout << "Elapsed: " << duration << " seconds" << std::endl;
+        }
+    } while(isVideo);
 
     return 0;
 }
